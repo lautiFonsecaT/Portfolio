1df0f1ecb6d9a7423d6dd01f0138ac95481d4e84
lautiftecchio <lautaro_fonseca@hotmail.com>
2025-05-30 12:45:33 +0200

    Se cambian colores de main, header y footer, buscando continuar colores de portada de linkedin

--- Sourse/js/components/about_me.js
+++ Sourse/js/components/about_me.js
@@ -1,9 +1,4 @@
 export const about_me= `
-            <section class="section_foto div_center">
-                <div class="div_container_foto div_center">
-                    <img src="./Sourse/img/foto_bienvenida.png" alt="foto de presentacion">
-                </div>
-            </section>
             <section class="section_introduccion div_center">
                 <div class="div_container_introduccion div_center">
                     <h1>Lautaro Fonseca Tecchio</h1>
